65434f6ff9c078e4de8f8ec22267b8d6068dd0a7
ishotjr <github@ishotjr.com>
2019-08-17 21:22:37 -0400

    :octocat: :tada: Initial commit

---
+++ src/IoTents.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "/home/ishotjr/dev/IoTents/src/IoTents.ino"
+/*
+ * Project IoTents
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/home/ishotjr/dev/IoTents/src/IoTents.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
